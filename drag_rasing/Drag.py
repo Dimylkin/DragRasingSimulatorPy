e7d66e5ad8704056143e07133df1bf392db29427
Dimylkin <sustavovds@icloud.com>
2025-10-20 20:53:44 +0500

    The mechanics of movement have been implemented and work has begun with the initial windows.

--- drag_rasing/Drag.py
+++ drag_rasing/Drag.py
@@ -9,7 +9,7 @@ from Window_Game import Maps
 pygame.init()
 
 FONT = pygame.font.Font(None, 36)
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 6006
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Драг рейсинг")
